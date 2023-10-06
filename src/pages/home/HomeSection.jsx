4b3e5db23c09f10d30c7db49b00628b36c58e4d7
filippennsater <filip.pennsater@hotmail.com>
2023-10-06 23:56:11 +0200

    text about TIE added

--- src/pages/home/HomeSection.jsx
+++ src/pages/home/HomeSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../App.css';
 import './HomeSection.scss';
-import Shares from '../../components/sharesETC/Shares';
 
 
 function HomeSection() {
@@ -17,41 +16,30 @@ function HomeSection() {
 
             
 
-            <div className='previewContainer'>
+            <div className='about-me-container'>
 
-                <ul className='share-list'>
+                <figure className="avatar" />
 
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
-                
-                
-                
-                
-               
-                
-               
-                
-               
+                <div className='about-text-container'>
+                    <p4>The infinite existence theory<br/><br/>
+Imagine throwing  2 dices, at some point when you throw you might get 1 and 1. If you keep throwing those two dice you will eventually get 1 and 1 again.
+Now imagine if you had 10 dice, you throw them, get a sequence (maybe all 1s) and eventually when you throw a lot more times you will probably get all 1s again.
+This would happen even with a million dice, billions... and even more. Especially if you had an infinite time to throw them!
+Now imagine instead that you threw sand, you would get a pattern...then had an infinite amount of tries, you could get the same pattern again...even a sandcastle.
 
-                </ul>
+Now instead of thinking about sand and dices, think about a dna sequence, memories, your entire makeup of atoms etc. If the universe had an infinite amount of time, it could easily create another you.
+Now also assume that the universe isn't eternally expanding, but actually expanding and later on shrinking and gathering into a single point again (like before the big bang).
+That would mean that the universe would be in an infinite state of exploding, expanding, contracting...over and over, almost like a dicetoss with an incredible amount of atoms.
+That would esentially mean that YOU will be created again and also with the exact same life you've lived. So what you experience now could actually have happened and will happen an infinite amount of time.
+And instead of thinking about multiversetheory that an infinite amount of universes would be an opportunity for an infinite amount of possibilities for how your own life could have been like. For example you could have been married to George Bush.
+Then that infinite amount of possible cenarios couldn't just be true in a multiverse, but also in your own universe in you own timeline giving an infinite amount of time.
+
+Now add a dark thought to the mix. If somehow your entire makeup right now or before your death. Could you start existing after an incredible amount of time in the future and you concious eternally present?
+<br/>
+
+</p4>
+
+                </div>
 
             </div>
 
